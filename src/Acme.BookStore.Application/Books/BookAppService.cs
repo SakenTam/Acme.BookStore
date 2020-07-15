8eee81cb9bcf62069810f34041acb236a7e13d93
SakenTam <sakentam@outlook.com>
2020-07-15 14:41:13 +0800

    FirstCommit

---
+++ src/Acme.BookStore.Application/Books/BookAppService.cs
@@ -0,0 +1,14 @@
+using System;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace Acme.BookStore.Books
+{
+    public class BookAppService:CrudAppService<Book,BookDto,Guid,PagedAndSortedResultRequestDto,CreateUpdateBookDto,CreateUpdateBookDto>
+    {
+        public BookAppService(IRepository<Book, Guid> repository) : base(repository)
+        {
+        }
+    }
+}
